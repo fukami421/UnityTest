7f940c0bcf9a68f23ccd14f9774d3409fab309de
fukami421 <fukaryu421@gmail.com>
2019-05-04 16:07:36 +0900

    added info about save

---
+++ Assets/Scripts/TestPlayerPrefs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerPrefs : MonoBehaviour// 変数の値をアプリケーションが終了しても残るようにするクラス
+{
+    #region fields
+    //private string highScore = "PLYAERLEVEL";
+    #endregion
+    #region methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        //データ名「CLEARSTAGE」に引数の0を代入        
+        PlayerPrefs.SetInt("PLYAERLEVEL", 0);
+        Debug.Log(LevelUp());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static int LevelUp()
+    {
+        int playerLevel = PlayerPrefs.GetInt("PLAYERLEVEL");
+        PlayerPrefs.SetInt("PLAYERLEVEL", playerLevel + 1);
+        return PlayerPrefs.GetInt("PLAYERLEVEL");
+    }
+    #endregion
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # UnityTest  
 ## Unityのテスト用プロジェクトです  
 
+## Unityのversion  
+2018.3.0f2  
+
+## 値のセーブ方法  
+https://qiita.com/situtyo666/items/e1a3bf15a2dccc2bfedc  
+
+
