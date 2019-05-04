77f5186f43cfec6fab602ad37fa807353f3ecd0e
fukami421 <fukaryu421@gmail.com>
2019-05-04 23:16:21 +0900

    could enjoy Scene manager

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//追加
+using UnityEngine.SceneManagement;
+ 
+public class ChangeScene : MonoBehaviour
+{
+
+    private string LoadSceneName = "A";
+    private AsyncOperation async;
+
+    // Use this for initialization
+    void Start()
+    {
+        //SceneManager.LoadScene("B", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(SceneManager.GetActiveScene().name == "A");
+            if (SceneManager.GetActiveScene().name == "A")
+            {
+                ScenePreLoad("B");
+            }else if (SceneManager.GetActiveScene().name == "B")
+            {
+                ScenePreLoad("A");
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) StartCoroutine("SceneLoad");
+    }
+
+    void ScenePreLoad(string changeScn)
+    {
+        async = SceneManager.LoadSceneAsync(changeScn, LoadSceneMode.Additive);
+        async.allowSceneActivation = false;
+    }
+
+    IEnumerator SceneLoad()
+    {
+        async.allowSceneActivation = true;
+        yield return null;
+        if (SceneManager.GetActiveScene().name == "A")
+        {
+            SceneManager.UnloadSceneAsync("A");
+        }
+        else if (SceneManager.GetActiveScene().name == "B")
+        {
+            SceneManager.UnloadSceneAsync("B");
+        }
+
+    }
+}
