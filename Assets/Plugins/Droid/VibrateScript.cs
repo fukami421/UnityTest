9769899f3126de431e0bbde35fb88aac568dcc02
fukami421 <fukaryu421@gmail.com>
2019-05-04 17:37:32 +0900

    could enjoy vibration

---
+++ Assets/Plugins/Droid/VibrateScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VibrateScript
+{
+    public static void vibrate(int msec)
+    {
+        AndroidJavaObject unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+
+        vibrator.Call("vibrate", msec);
+
+        vibrator.Dispose();
+        currentActivity.Dispose();
+        unityPlayer.Dispose();
+    }
+}
--- Assets/Scripts/MakeVibration.cs
+++ Assets/Scripts/MakeVibration.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 /// <summary>
 /// VibrationをplaySystemSound.mmと共に実装しています
-/// 参考: https://qiita.com/DURIAN_JADE/items/3fdddb4ad19658f50539
+/// 参考(iOS): https://qiita.com/DURIAN_JADE/items/3fdddb4ad19658f50539
+/// 参考(Droid): https://ameblo.jp/vantrux/entry-11605689738.html
 /// ネイティブコードと連携する
 /// 参考: https://qiita.com/edo_m18/items/2aff04ec51c7ffc4cf7c
 /// </summary>
@@ -25,11 +26,13 @@ public class MakeVibration : MonoBehaviour
 
     private static void Vibration()
     {
-        #if UNITY_EDITOR
-            Debug.Log("Play system sound or vibration on real devices");
-        #else
-            playSystemSound(1519);
-        #endif
+#if UNITY_EDITOR
+        Debug.Log("Play system sound or vibration on real devices");
+#elif UNITY_IPHONE
+        playSystemSound(1519);//Plugins/iOS/VplaySystemSound.mmを呼び出している
+#else
+        VibrateScript.vibrate(3);//Plugins/Droid/VibrateScriptを呼び出している
+#endif
     }
     #endregion
 }
